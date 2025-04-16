c607376a3612428e8d403452a27e81d537273c11
Mudit Dargar <muditdargar@gmail.com>
2025-04-17 00:07:21 +0530

    new changes

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -4,12 +4,13 @@ import { connectDB } from '@/db/config';
 import CampaignModel from '@/models/campaign-model';
  import DonationModal from '@/models/donation-model';
 import React from 'react'
+import CampaignsTable from '../campaigns/_components/campaign-table';
 
 connectDB()
 
 async function Dashboardpage() {
 
-  const [campaignsCount , donationsCount , amountRaised] = await Promise.all([
+  let [campaignsCount , donationsCount , amountRaised] = await Promise.all([
     CampaignModel.countDocuments({}),
     DonationModal.countDocuments({}),
     DonationModal.aggregate([
@@ -23,6 +24,19 @@ async function Dashboardpage() {
   ])
 
 
+  console.log("campaigns count", campaignsCount);
+  console.log("donations count", donationsCount);
+  console.log("amount raised", amountRaised);
+
+
+  amountRaised =  amountRaised[0]?.totalAmount || 0 ;
+
+  const [recentCampaigns , recentDonations] = await Promise.all([
+    CampaignModel.find({}).sort({createdAt : -1 }).limit(5),
+    DonationModal.find({}).sort({createdAt : -1 }).limit(5),
+  ])
+
+
   return (
     <div>
       <PageTitle title="Dashboard" />
@@ -31,20 +45,29 @@ async function Dashboardpage() {
       <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-10"> 
         <DashboardCard  cardTitle='campaigns' 
         description='Total number of campaigns including active and inactive' 
-        value='100'
+        value={campaignsCount.toString()}
         onClickPath = '/admin/campaigns'
         />
 
 <DashboardCard  cardTitle='Donations' 
         description='Total number of donations done by users for all campaigns' 
-        value='69'
+        value={donationsCount.toString()}
         onClickPath = '/admin/donations'
         />
 
 <DashboardCard  cardTitle='Amount Raised' 
         description='Total amount raised by all campaigns   including active and inactive' 
-        value='₹ 774'
+        value={`₹ ${amountRaised.toString()}`}
         />
+      </div>
+
+      <div
+      className='mt-10'
+      >
+  <h1 className='text-xl font-bold text-gray-600 mb-5'>
+    Recent Campaigns
+    </h1>
+    <CampaignsTable campaigns={JSON.parse(JSON.stringify(recentCampaigns))}/>
       </div>
     </div>
   )
